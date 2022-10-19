93ce31a0ed5feb8af7368554328e74dc49415aeb
marcelmacedo <marcel.handock@gmail.com>
2022-10-19 19:40:02 -0400

    first commit

---
+++ src/main/java/curso/api/rest/CursospringrestapiApplication.java
@@ -0,0 +1,39 @@
+package curso.api.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication													// Saber que é uma aplicação SpringBoot iniciar os recursos
+@EntityScan(basePackages = {"curso.api.rest.model"})					// Varer as pastas de Model.
+@ComponentScan(basePackages = {"curso.*"})								// Vare todas as pastas que tem no projeto
+@EnableJpaRepositories(basePackages = {"curso.api.rest.repository"})	// Local do repository, interfaces de persistencias
+@EnableTransactionManagement   											// Controla transação no banco de dados.
+@EnableWebMvc 															// Habilita recursos MVC
+@RestController															// O projeto roda rest controller e retorna Json.
+@EnableAutoConfiguration												// Spring configura todo projeto
+public class CursospringrestapiApplication implements WebMvcConfigurer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringrestapiApplication.class, args);
+	}
+	
+	/*Controle mapeamento global para restringir acesso a API*/
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		
+		registry.addMapping("/usuario/**") /* Liberar tudo que estiver mapeado */
+		.allowedMethods("*") /* Pode restringir quais metodos POST", "PUT", "DELETE pode ou nao ser acessado */
+		.allowedOrigins("*"); /* Pode restringir qual ou quais servidores podem ter acesso "www.jdevtreinamento.com.br", "localhost:8080"*/
+		/* Liberando mapeamento para todas as origens */
+	}
+
+}
---
+++ src/main/java/curso/api/rest/controller/IndexController.java
@@ -0,0 +1,98 @@
+package curso.api.rest.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import curso.api.rest.model.Usuario;
+import curso.api.rest.repository.UsuarioRepository;
+
+@CrossOrigin(origins = "*") /*Controle de acesso a API*/
+@RestController /* Arquitetura REST */
+@RequestMapping(value = "/usuario")
+public class IndexController {
+	
+	/* Serviço RestFul*/
+	/*
+	 * @GetMapping(value = "/", produces = "application/json") 
+	 * public ResponseEntity init() { 
+	 * 		return new ResponseEntity("Ola Spring Rest API", HttpStatus.OK); 
+	 * }
+	 */
+
+	@Autowired /*Se fosse CDI seria @Inject*/
+	private UsuarioRepository usuarioRepository;
+
+	
+	/*Serviço RESTfull*/
+	@GetMapping(value = "/{id}/relatoriopdf", produces = "application/pdf")
+	public ResponseEntity<Usuario> relatorio(@PathVariable(value = "id") Long id) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+		
+	
+	/*Serviço RESTfull*/
+	@GetMapping(value = "/{id}", produces = "application/json")
+	public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) {
+		
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/", produces = "application/json")
+	public ResponseEntity<List<Usuario>> usuario(){
+		
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+		
+		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario){
+		
+		for(int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
+	@PutMapping(value = "/", produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
+		
+		for(int pos = 0; pos < usuario.getTelefones().size(); pos++) {
+			usuario.getTelefones().get(pos).setUsuario(usuario);
+		}
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}", produces = "application/text")
+	public String deletar(@PathVariable(value = "id") Long id) {
+		
+		usuarioRepository.deleteById(id);
+		
+		return "Usuario deletado!";
+	}
+
+}
