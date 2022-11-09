f0694d22438b4e87a3b08a49490f93d2ac452c5d
marcelmacedo <marcel.handock@gmail.com>
2022-11-09 12:57:25 -0400

    Spring Security - Configurando usuario

--- src/main/java/curso/api/rest/model/Usuario.java
+++ src/main/java/curso/api/rest/model/Usuario.java
@@ -1,19 +1,28 @@
 package curso.api.rest.model;
 
-import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.CascadeType;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-public class Usuario implements Serializable {
+public class Usuario implements UserDetails {
 
 	private static final long serialVersionUID = 1L;
 
@@ -27,8 +36,27 @@ public class Usuario implements Serializable {
 	
 	private String nome;
 	
-	@OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL, fetch = FetchType.LAZY)
 	private List<Telefone> telefones = new ArrayList<Telefone>();
+	
+	@OneToMany(fetch = FetchType.EAGER)
+	/* Create new table "usuarios_role" */
+	/* NOTATION uniqueConstraints = @UniqueConstraint with constraint name "unique_role_user"*/ 
+	/* joinColumns irá unir a tabela usuario e inverseJoinColumns ira unir com a tabela role */
+	/* inverseJoinColumns   */
+	@JoinTable(name = "usuarios_role", 
+		uniqueConstraints = @UniqueConstraint(
+					columnNames = {"usuario_id", "role_id"}, name = "unique_role_user"
+					), 
+					joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", unique = false,
+						foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)
+					),
+					inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", table = "role", unique = false, updatable = false,
+						foreignKey = @ForeignKey(name = "role_fk", value = ConstraintMode.CONSTRAINT)
+					)
+				)
+	private List<Role> roles; /* São os papeis de permissão de  acessos */
+	
 	
 	public List<Telefone> getTelefones() {
 		return telefones;
@@ -85,5 +113,49 @@ public class Usuario implements Serializable {
 			return false;
 		Usuario other = (Usuario) obj;
 		return Objects.equals(id, other.id);
+	}
+
+	/* Metodos padrão do Spring Security  */
+	/* getAuthorities são as autorizações */
+	/* São os acessos do usuario. Ex.: ROLE_ADMIN, ROLE_VISITANTE */
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return roles;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 }
